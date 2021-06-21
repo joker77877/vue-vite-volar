aae001dae7c19620193b53f0dfbdf9421aee89e7
尹彬宇 <joker7778777@163.com>
2021-06-21 15:52:03 +0800

    feat(提交): 提交文件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="tsx">
+import { defineComponent, onBeforeMount, reactive } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+    name: 'App',
+    components: {
+        HelloWorld,
+    },
+    setup() {
+        /**
+         * @description: 组件状态
+         */
+        const state = reactive({
+            columns: [
+                {
+                    title: '操作',
+                    width: 250,
+                    customRender: () => (
+                        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite">123</HelloWorld>
+                    ),
+                },
+            ],
+        });
+
+        return {};
+    },
+});
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 import * as volar from '@volar/experimental/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(volar.vitePluginOptions)],
-})
+    plugins: [
+        vue(volar.vitePluginOptions),
+        vueJsx({
+            enableObjectSlots: false,
+            optimize: true,
+        }),
+    ],
+});
